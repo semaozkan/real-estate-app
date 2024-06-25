461236426ad25f39d7334bb6dff02444235bb600
Semanur Özkan <semanurozkan4444@gmail.com>
2024-06-25 16:58:03 +0300

    feat(home): create hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import "./layout.scss"
-import Navbar from "./components/navbar/Navbar" 
+import "./layout.scss";
+import Navbar from "./components/navbar/Navbar";
+import HomePage from "./routes/homePage/homePage";
 
 function App() {
   return (
     <div className="layout">
-      <Navbar/>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
