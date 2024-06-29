7e0751693b99d8c5ea4691070d6a37033f8ca45a
Semanur Özkan <semanurozkan4444@gmail.com>
2024-06-30 00:06:19 +0300

    feat(listPage): create list page

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,7 +1,24 @@
+import Filter from "../../components/filter/Filter";
+import { listData } from "../../lib/dummydate";
 import "./listPage.scss";
+import Card from "./../../components/card/Card";
 
 const ListPage = () => {
-  return <div className="listPage">ListPage</div>;
+  const data = listData;
+
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">Map</div>
+    </div>
+  );
 };
 
 export default ListPage;
