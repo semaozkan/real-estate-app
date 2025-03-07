5d68b30fa210cd5aa1d7d803955b81ca39686fa4
Semanur Özkan <semanurozkan4444@gmail.com>
2025-03-07 15:09:02 +0300

    feat(controller): create auth controller

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+
+export const register = (req, res) => {
+  //db operations
+};
+
+export const login = (req, res) => {
+  //db operations
+};
+
+export const logout = (req, res) => {
+  //db operations
+};
