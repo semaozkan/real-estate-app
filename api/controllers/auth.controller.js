ae0240718881c3778c47997b843fc32fd3bc05b4
Semanur Özkan <semanurozkan4444@gmail.com>
2025-03-09 12:42:19 +0300

    feat(auth-controller): add register function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,33 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
-export const register = (req, res) => {
+export const register = async (req, res) => {
   //db operations
+  const { username, email, password } = req.body;
+
+  try {
+    // HASH THE PASSWORD
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    console.log(hashedPassword);
+
+    // CREATE A NEW USER AND SAVE IT TO DO DB
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    console.log(newUser);
+
+    res.status(201).json({ message: "User crested succesfuly" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to create user!" });
+  }
 };
 
 export const login = (req, res) => {
